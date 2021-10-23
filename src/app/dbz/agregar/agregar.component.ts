159df9932505273212b46e3789735e2999822f3e
David Torres <davidntorres1999@gmail.com>
2021-10-23 18:53:24 -0300

    @Outputs y EventEmmiter

--- src/app/dbz/agregar/agregar.component.ts
+++ src/app/dbz/agregar/agregar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
 
 @Component({
@@ -7,26 +7,24 @@ import { Personaje } from '../interfaces/dbz.interface';
 })
 export class AgregarComponent {
 
-  @Input() personajes : Personaje [] = [];
-
   @Input() nuevo : Personaje = {
     nombre: '',
     poder: 0
   }
 
+  @Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
+
   agregar(){
     if(this.nuevo.nombre.trim().length === 0){ return }
 
     console.log(this.nuevo);
-
-    this.personajes.push(this.nuevo);
+    this.onNuevoPersonaje.emit( this.nuevo );
 
     this.nuevo = {
       nombre: '',
       poder: 0
     }
 
-    console.log(this.personajes);
   }
-
+  
 }
